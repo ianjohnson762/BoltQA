9ef6fcdd7876051ca53e2c05309872c8000b83c3
Jakub Zochowski <Jakub Zochowski>
2017-06-29 15:29:41 +0100

    Created Reg Form

--- BoltQA/BoltQA/Form1.Designer.cs
+++ BoltQA/BoltQA/Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Find = new System.Windows.Forms.Button();
+            this.btn_Register = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Find
+            // 
+            this.btn_Find.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Find.Location = new System.Drawing.Point(26, 70);
+            this.btn_Find.Name = "btn_Find";
+            this.btn_Find.Size = new System.Drawing.Size(80, 36);
+            this.btn_Find.TabIndex = 0;
+            this.btn_Find.Text = "Find";
+            this.btn_Find.UseVisualStyleBackColor = true;
+            // 
+            // btn_Register
+            // 
+            this.btn_Register.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Register.Location = new System.Drawing.Point(26, 28);
+            this.btn_Register.Name = "btn_Register";
+            this.btn_Register.Size = new System.Drawing.Size(80, 36);
+            this.btn_Register.TabIndex = 0;
+            this.btn_Register.Text = "Register";
+            this.btn_Register.UseVisualStyleBackColor = true;
+            this.btn_Register.Click += new System.EventHandler(this.btn_Register_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 317);
+            this.Controls.Add(this.btn_Register);
+            this.Controls.Add(this.btn_Find);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Find;
+        private System.Windows.Forms.Button btn_Register;
     }
 }
 
--- BoltQA/BoltQA/Registration.cs
+++ BoltQA/BoltQA/Registration.cs
@@ -10,23 +10,21 @@ using System.Windows.Forms;
 
 namespace BoltQA
 {
-    public partial class Form1 : Form
+    public partial class Registration : Form
     {
-        public Form1()
+        public Registration()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Registration_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void btn_Register_Click(object sender, EventArgs e)
+        private void btn_Cancel_Click(object sender, EventArgs e)
         {
-            Registration reg = new Registration();
-            reg.ShowDialog();
-
+            Close();
         }
     }
 }
